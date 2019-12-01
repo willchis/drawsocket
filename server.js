1da92528a523f04253a166ecb8bd758d406faec9
William Chislett <willchis@hotmail.com>
2019-11-30 16:44:55 -0800

    Adding heroku port.

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-var express = require('express'), 
+
+const port = process.env.PORT || 8080;
+
+const express = require('express'), 
     app = express(),
     http = require('http'),
     socketIo = require('socket.io');
 
 // start webserver on port 8080
-var server =  http.createServer(app);
-var io = socketIo.listen(server);
-server.listen(8080);
+const server =  http.createServer(app);
+const io = socketIo.listen(server);
+server.listen(port);
 // add directory with our static files
 app.use(express.static(__dirname + '/public'));
-console.log("Server running on 127.0.0.1:8080");
+console.log("Server running on port:" + port);
 
 // array of all lines drawn
-var line_history = [];
+let line_history = [];
 
 // event-handler for new incoming connections
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
 
    // first send the history to the new client
    for (var i in line_history) {
@@ -23,7 +26,7 @@ io.on('connection', function (socket) {
    }
 
    // add handler for message type "draw_line".
-   socket.on('draw_line', function (data) {
+   socket.on('draw_line', (data) => {
       // add received line to history 
       line_history.push(data.line);
       // send line to all clients
